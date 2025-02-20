8fda5e29eab478006176b0e15fe57f18faf82fa1
KChoudrey <kchoudrey@gmail.com>
2025-02-20 20:24:56 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// Smooth Scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
 // Contact Form Submission
 document.getElementById('contactForm').addEventListener('submit', function(event) {
   event.preventDefault();
